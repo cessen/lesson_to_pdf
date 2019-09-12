913ff386e24ec6e2af07101ed61d09978a637aa1
Nathan Vegdahl <cessen@cessen.com>
2019-09-12 17:22:00 +0900

    Process markdown file before converting to html.
    This let's the markdown files be pure markdown without any embedded
    html.

--- lesson_to_pdf.py
+++ lesson_to_pdf.py
@@ -3,23 +3,58 @@
 import sys
 import os
 import subprocess
+import string
+import random
+
 
 if __name__ == "__main__":
     md_filename = sys.argv[1]
     html_filename = md_filename.rsplit(".")[0] + ".html"
     pdf_filename = md_filename.rsplit(".")[0] + ".pdf"
+    tmp_md_filename = ''.join(
+        random.choices(
+            string.ascii_lowercase + string.ascii_uppercase + string.digits,
+            k=16)
+        ) + ".md"
 
+    # Create a modified md file
+    with open(tmp_md_filename, 'w') as mod_md_file:
+        with open(md_filename) as md_file:
+            state = 0
+            for line in md_file:
+                if line.startswith("---") and state == 0:
+                    mod_md_file.write('<div id="header"><div>\n')
+                    state += 1
+                elif line.startswith("## ") and state == 1:
+                    mod_md_file.write(line)
+                    state +=1
+                elif line.startswith("## ") and state == 2:
+                    mod_md_file.write("</div><div>\n")
+                    mod_md_file.write(line)
+                    state +=1
+                elif line.startswith("---") and state == 3:
+                    mod_md_file.write('</div></div>\n')
+                    state += 1
+                else:
+                    mod_md_file.write(line)
+            if state != 4:
+                print("Error!  File does not follow the lesson plan format.")
+                exit()
+
+
+    # Generate the html
     subprocess.Popen([
         "pandoc",
         "-s",
         "-M",
         "title:none",
         '--css=../plan.css',
-        md_filename,
+        tmp_md_filename,
         "-o",
         html_filename,
     ]).wait()
     
+    # Generate the pdf
     subprocess.Popen(
         [
             "wkhtmltopdf",
@@ -30,4 +65,5 @@ if __name__ == "__main__":
         ],
     ).wait()
 
+    os.remove(tmp_md_filename)
     os.remove(html_filename)
